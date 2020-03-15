00c564a962b61c7e9747449f063e82707296163b
bakunobu <bakunobu@gmail.com>
2020-03-15 22:00:46 +0300

    func added

--- black_scholes_merton.py
+++ black_scholes_merton.py
@@ -62,5 +62,38 @@ def bsm_vega(S0, K, T, r, sigma):
     d1 = (log(S0 / K) + (r + 0.5 * sigma ** 2) * T) / (sigma * sqrt(T))
     vega = S0 * stats.norm.cdf(d1, 0.0, 1.0) * sqrt(T)
     return(vega)
+
+def bsm_call_imp_vol(S0, K, T, r, C0, sigma_est, it=100):
+    """
+    Implied volatility of Eur call option in BSM model
+
+    
+    Params:
+    =======
+    S0 : float
+        initial stock/index level
+    K : float
+        strike price
+    T : float
+        maturity date (in year fractions)
+    r : float
+        constant risk-free short rate
+    C0 : int ???
+        initial quote???
+    sigma_est : float
+        extimate of imp. volatility
+    it : int
+        number of iterations
+    
+    Returns
+    =======
+    sigma_est : float
+        numerically estimated implied volatility
+    """
+
+    for i in range(it):
+        sigma_est -= ((bsm_call_value(S0, K, T, r, sigma_est) - C0) / bsm_vega(S0, K, T, r, sigma_est))
+    return(sigma_est)
+    
           
     
