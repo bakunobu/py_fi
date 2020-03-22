f1aae4b339303387c0f10a2afad736e67a7269f6
bakunobu <bakunobu@gmail.com>
2020-03-22 21:43:51 +0300

    new Series and the Regime func added

--- tech_analysis_42.py
+++ tech_analysis_42.py
@@ -21,9 +21,9 @@ def rolling_window(df, rolling_col, new_col_name, window):
     Vars
     ====
     df: Pandas DataFrame type object
-        DataFrame with a specified col name (df['col_name'])
+        DataFrame for meanipulation
     rolling_col: str
-        column the prediction is based on
+        column the prediction is based on (no check added)
     new_col_name: str
         a name of a new olumn with predicted data
     window: int
@@ -40,5 +40,30 @@ def rolling_window(df, rolling_col, new_col_name, window):
     df[new_col_name] = np.round(df[rolling_col].rolling(window).mean(), 2)
     return(df)
 
+
 sp500_42d = rolling_window(sp500, 'Close', '42d', 42)
-print(sp500_42d['42d'].tail(10))
+sp500_pred = rolling_window(sp500_42d, 'Close', '252d', 252)
+
+#print(sp500_pred[['Close', '42d', '252d']].tail(10))
+#sp500_pred[['Close', '42d', '252d']].plot(grid=True, figsize=(8, 5))
+
+#plt.show()
+
+sp500_pred['42-252'] = sp500_pred['42d'] - sp500_pred['252d']
+#print(sp500_pred['42-252'].tail())
+
+#Regime func
+SD = 50
+sp500_pred['Regime'] = np.where(sp500_pred['42-252'] > SD, 1, 0)
+sp500_pred['Regime'] = np.where(sp500_pred['42-252'] < -SD, - 1, sp500_pred['Regime'])
+#print(sp500_pred['Regime'].value_counts())
+
+#sp500_pred['Regime'].plot(lw=1.5)
+#plt.ylim([-1.1, 1.1])
+#plt.show()
+
+sp500_pred['Market'] = np.log(sp500_pred['Close'] / sp500_pred['Close'].shift(1))
+sp500_pred['Strategy'] = sp500_pred['Regime'].shift(1) * sp500_pred['Market']
+
+sp500_pred[['Market', 'Strategy']].cumsum().apply(np.exp).plot(grid=True, figsize=(8,5))
+plt.show()
